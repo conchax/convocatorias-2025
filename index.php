a8ee34a276a7f1b81b5b949706c52231ad429a2d
Gonzalo Torres <conchax@gmail.com>
2025-04-21 00:19:28 -0600

    produccion

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ $secB = 1;
 $secC = 2;
 $convoActivo = 3;
 
-if ($_SERVER['PHP_AUTH_USER']!="PrepaLSEP" || $_SERVER['PHP_AUTH_PW']!="#dT/$%lf$229820%&#$")
+/* if ($_SERVER['PHP_AUTH_USER']!="PrepaLSEP" || $_SERVER['PHP_AUTH_PW']!="#dT/$%lf$229820%&#$")
 {
 header('WWW-Authenticate: Basic realm="Ingrese su usario y contraseña asignada"');
 header('HTTP/1.0 401 Unauthorized');
 echo 'Authorization Required To Server.';
 exit;
-}
+} */
 ?>
 
 <!doctype html>

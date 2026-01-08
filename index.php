7d902d7f5488223709e06812c93c303c768e9db7
gonzalo torres <conchax@gmail.com>
2026-01-08 00:25:55 -0600

    85 proxima

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ $secB = 9;
 $secC = 10;
 $convoActivo = 11;
 
-if ($_SERVER['PHP_AUTH_USER'] != "PrepaLSEP" || $_SERVER['PHP_AUTH_PW'] != "convo85prox") {
+/* if ($_SERVER['PHP_AUTH_USER'] != "PrepaLSEP" || $_SERVER['PHP_AUTH_PW'] != "convo85prox") {
     header('WWW-Authenticate: Basic realm="Ingrese su usario y contraseña asignada"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Authorization Required To Server.';
     exit;
-}
+} */
 ?>
 
 <!doctype html>
